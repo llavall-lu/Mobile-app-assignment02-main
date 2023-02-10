b52edc29a9d7b4aa0b5f78abd108d10a88a89356
llavall-lu <llavall.lu@gmail.com>
2023-02-10 11:24:44 +0000

    Sign in design Complete

---
+++ app/src/main/java/com/example/ticketapplication/WelcomeActivity.java
@@ -0,0 +1,20 @@
+package com.example.ticketapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+
+    public void register(View view) {
+        startActivity(new Intent(WelcomeActivity.this,RegistrationActivity.class));
+    }
+}
